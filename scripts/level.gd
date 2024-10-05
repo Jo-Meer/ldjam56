e60889f3f4c0c64797d9606a245509301645d71d
Jonas Meer <meerdev@mailbox.org>
2024-10-05 21:37:01 +0200

    add avatar to base elements split

--- scripts/level.gd
+++ scripts/level.gd
@@ -1,2 +1,96 @@
 extends Node2D
 
+@export var avatar: Player
+
+var fire: Player
+var water: Player
+var air: Player
+var earth: Player
+
+var mud: Player
+var light: Player
+var metal: Player
+var steam: Player
+var ice: Player
+var sand: Player
+
+var active_creature: Player
+
+
+
+func _ready():
+	active_creature = avatar
+	# fire = instantiate(preload("res://creatures/avatar.tscn"))
+	# water = instantiate(preload("res://creatures/avatar.tscn"))
+	# air = instantiate(preload("res://creatures/avatar.tscn"))
+	# earth = instantiate(preload("res://creatures/avatar.tscn"))
+	#
+	# mud = instantiate(preload("res://creatures/avatar.tscn"))
+	# light = instantiate(preload("res://creatures/avatar.tscn"))
+	# metal = instantiate(preload("res://creatures/avatar.tscn"))
+	# steam = instantiate(preload("res://creatures/avatar.tscn"))
+	# ice = instantiate(preload("res://creatures/avatar.tscn"))
+	# sand = instantiate(preload("res://creatures/avatar.tscn"))
+
+
+	pass
+
+func _physics_process(delta: float) -> void:
+	if Input.is_action_just_pressed("split"):
+		if active_creature == avatar:
+			fire = instantiate(preload("res://creatures/avatar.tscn"))
+			water = instantiate(preload("res://creatures/avatar.tscn"))
+			air = instantiate(preload("res://creatures/avatar.tscn"))
+			earth = instantiate(preload("res://creatures/avatar.tscn"))
+		
+			avatar.queue_free()
+			move_control_to(fire)
+
+	elif Input.is_action_just_pressed("next_creature"):
+		move_control_to(get_next(active_creature))
+	elif Input.is_action_just_pressed("prev_creature"):
+		move_control_to(get_prev(active_creature))
+			
+
+
+func get_next(base: Player) -> Player:
+	var all: Array[Player] = [fire, water, air, earth, mud, light, metal, steam, ice, sand]
+
+	var active_index = all.find(base)
+
+	for i in range(active_index, active_index + all.size()):
+		var current = (i + 1) % all.size()
+
+		if all[current]:
+			return all[current]
+
+	return active_creature
+
+func get_prev(base: Player) -> Player:
+	var all: Array[Player] = [fire, water, air, earth, mud, light, metal, steam, ice, sand]
+	all.reverse()
+
+	var active_index = all.find(base)
+
+	for i in range(active_index, active_index + all.size()):
+		var current = (i + 1) % all.size()
+
+		if all[current]:
+			return all[current]
+
+	return active_creature
+
+func instantiate(creature: PackedScene) -> Player:
+	var instance = creature.instantiate()
+	instance.position = avatar.position
+	instance.deactivate()
+	add_child(instance)
+	return instance
+
+
+func move_control_to(player: Player):
+	if player == active_creature:
+		return
+	active_creature.deactivate()
+	player.activate()
+	active_creature = player
--- scripts/player_controller.gd
+++ scripts/player_controller.gd
@@ -1,3 +1,5 @@
+class_name Player
+
 extends CharacterBody2D
 
 
@@ -6,11 +8,23 @@ extends CharacterBody2D
 
 @export var type: Globals.Type = Globals.Type.AVATAR
 
+signal deactivated
+signal activated
+
+var is_active = true
 
 func _physics_process(delta: float) -> void:
 	# Add the gravity.
 	if not is_on_floor():
 		velocity += get_gravity() * delta
+	
+
+	if is_active == false:
+		# break and handle gravity
+		velocity.x = move_toward(velocity.x, 0, SPEED)
+		move_and_slide()
+		return
+
 
 	# Handle jump.
 	if Input.is_action_just_pressed("jump") and is_on_floor():
@@ -25,3 +39,13 @@ func _physics_process(delta: float) -> void:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
 
 	move_and_slide()
+
+func activate():
+	is_active = true
+	activated.emit()
+	$Camera2D.enabled = true
+
+func deactivate():
+	is_active = false
+	deactivated.emit()
+	$Camera2D.enabled = false
