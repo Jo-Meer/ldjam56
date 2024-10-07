c9a02cd1e158c2146bf17b640fb0d8f859b0b6ad
Ntimi Schnütgen <ntimi.schnuetgen@gmx.de>
2024-10-07 23:32:48 +0200

    credits and main menu

---
+++ scenes/credits.gd
@@ -0,0 +1,49 @@
+extends Node2D
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	do_credits();
+
+
+func do_credits():
+	$ColorRect.visible = true;
+	$Button.modulate = Color.TRANSPARENT;
+	$ENDTEXT.modulate = Color.TRANSPARENT;
+	$SCROLLER.modulate = Color.TRANSPARENT;
+	
+	var fade_tween = create_tween();
+	fade_tween.tween_property($ColorRect, "modulate", Color.TRANSPARENT, 2);
+	await fade_tween.finished;
+	
+	var endtext_tween = create_tween();
+	$ENDTEXT.show();
+	endtext_tween.tween_property($ENDTEXT, "modulate", Color.WHITE, 2);
+	await endtext_tween.finished;
+	
+	await get_tree().create_timer(2).timeout;
+	
+	var button_tween = create_tween();
+	button_tween.tween_property($Button, "modulate", Color.WHITE, 1);
+	
+	await get_tree().create_timer(1).timeout;
+	
+	var endtext_tween2 = create_tween();
+	endtext_tween2.tween_property($ENDTEXT, "modulate", Color.TRANSPARENT, 2);
+	await endtext_tween2.finished;
+	
+	var scoller_tween = create_tween();
+	$SCROLLER.show();
+	scoller_tween.tween_property($SCROLLER, "modulate", Color.WHITE, 2);
+	await scoller_tween.finished;
+	
+	await get_tree().create_timer(2).timeout;
+	
+	var scoll_tween = create_tween();
+	scoll_tween.tween_property($SCROLLER, "position", $SCROLLER.position - Vector2(0, 3000), 40);
+	await scoll_tween.finished;
+	
+
+
+func _on_button_pressed() -> void:
+	get_tree().change_scene_to_file("res://scenes/MainMenue.tscn");
--- scripts/main_menue.gd
+++ scripts/main_menue.gd
@@ -6,6 +6,7 @@ func _ready() -> void:
 func run_intro():
 	$ColorRect.visible = true;
 	$Button.modulate = Color.TRANSPARENT;
+	$CreditsButton.modulate = Color.TRANSPARENT;
 	
 	var fade_tween = create_tween();
 	fade_tween.tween_property($ColorRect, "modulate", Color.TRANSPARENT, 2);
@@ -13,6 +14,14 @@ func run_intro():
 	await get_tree().create_timer(0.5).timeout;
 	var tween = create_tween();
 	tween.tween_property($Button, "modulate", Color.WHITE, 2);
+	
+	await get_tree().create_timer(5).timeout;
+	var credits_tween = create_tween();
+	credits_tween.tween_property($CreditsButton, "modulate", Color.WHITE, 2);
 
 func start_game():
 	SceneManager.start()
+
+
+func _on_credits_button_pressed() -> void:
+	get_tree().change_scene_to_file("res://scenes/Credits.tscn");
